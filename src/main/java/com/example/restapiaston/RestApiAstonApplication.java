f8e682660668a1d60f295caab8bcf090450d970b
Anastasiya Vusik <nastyavusik13@gmail.com>
2024-06-24 15:09:56 +0300

    first commit

---
+++ src/main/java/com/example/restapiaston/RestApiAstonApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapiaston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAstonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiAstonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restapiaston/RestApiAstonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapiaston;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAstonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
